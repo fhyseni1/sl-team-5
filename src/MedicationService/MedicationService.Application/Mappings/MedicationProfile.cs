48a8588c26a9e8e49509ac607b4540b9692466f6
Arben Syla <arbenbsyla1@gmail.com>
2025-10-16 14:23:54 +0200

    feat(medication): add automapper profile for medications
    Configure AutoMapper mappings for medication DTOs and entities.
    
    Mappings:
    - Medication -> MedicationResponseDto (with calculated properties)
    - MedicationCreateDto -> Medication
    - MedicationUpdateDto -> Medication
    
    Features:
    - Calculated properties (ActiveSchedulesCount, UpcomingDosesCount)
    - Proper property ignoring for entity relationships
    - Timestamp management exclusions
    
    Location: MedicationService.Application/Mappings/MedicationProfile.cs

---
+++ src/MedicationService/MedicationService.Application/Mappings/MedicationProfile.cs
@@ -0,0 +1,40 @@
+using AutoMapper;
+using MedicationService.Application.DTOs.Medications;
+using MedicationService.Domain.Entities;
+
+namespace MedicationService.Application.Mappings
+{
+    public class MedicationProfile : Profile
+    {
+        public MedicationProfile()
+        {
+            CreateMap<Medication, MedicationResponseDto>()
+                .ForMember(dest => dest.ActiveSchedulesCount,
+                    opt => opt.MapFrom(src => src.Schedules.Count(s => s.IsActive)))
+                .ForMember(dest => dest.UpcomingDosesCount,
+                    opt => opt.MapFrom(src => src.Doses.Count(d => !d.IsTaken && d.ScheduledTime > DateTime.UtcNow)));
+
+            CreateMap<MedicationCreateDto, Medication>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Status, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.ScannedImageUrl, opt => opt.Ignore())
+                .ForMember(dest => dest.Schedules, opt => opt.Ignore())
+                .ForMember(dest => dest.Doses, opt => opt.Ignore())
+                .ForMember(dest => dest.DrugInteractions, opt => opt.Ignore())
+                .ForMember(dest => dest.Prescription, opt => opt.Ignore());
+
+            CreateMap<MedicationUpdateDto, Medication>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Schedules, opt => opt.Ignore())
+                .ForMember(dest => dest.Doses, opt => opt.Ignore())
+                .ForMember(dest => dest.DrugInteractions, opt => opt.Ignore())
+                .ForMember(dest => dest.Prescription, opt => opt.Ignore());
+        }
+    }
+}
+
