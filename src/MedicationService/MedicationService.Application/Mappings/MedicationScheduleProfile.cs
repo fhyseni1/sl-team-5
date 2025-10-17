e08cf325407791ffec8535ef68d7c7b4b7542014
EnesKllokoqi1 <eneskllokoqi@gmail.com>
2025-10-18 00:26:17 +0200

    Work in progress...

---
+++ src/MedicationService/MedicationService.Application/Mappings/MedicationScheduleProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MedicationService.Application.DTOs.Schedules;
+using MedicationService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicationService.Application.Mappings
+{
+    public class MedicationScheduleProfile : Profile
+    {
+        public MedicationScheduleProfile()
+        {
+   
+            CreateMap<MedicationSchedule, ScheduleResponseDto>()
+               .ForMember(dest => dest.MedicationName, opt => opt.MapFrom(src => src.Medication.Name));
+
+            CreateMap<ScheduleCreateDto, MedicationSchedule>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())          
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true)) 
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Medication, opt => opt.Ignore()) 
+                .ForMember(dest => dest.Reminders, opt => opt.Ignore());
+
+            CreateMap<ScheduleUpdateDto, MedicationSchedule>()
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(_ => DateTime.UtcNow))
+                .ForMember(dest => dest.Medication, opt => opt.Ignore())
+                .ForMember(dest => dest.Reminders, opt => opt.Ignore());
+
+        }
+    }
+}
+
---
+++ src/MedicationService/MedicationService.Infrastructure/Repositories/MedicationScheduleRepository.cs
@@ -0,0 +1,118 @@
+﻿using MedicationService.Application.Interfaces;
+using MedicationService.Domain.Entities;
+using MedicationService.Domain.Enums;
+using MedicationService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicationService.Infrastructure.Repositories
+{
+    public class MedicationScheduleRepository : IMedicationScheduleRepository
+    {
+        public readonly MedicationDbContext _dbContext;
+        public MedicationScheduleRepository(MedicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<MedicationSchedule> AddAsync(MedicationSchedule entity)
+        {
+            var medicationSchedule = new MedicationSchedule
+            {
+                Id = Guid.NewGuid(),
+                MedicationId = entity.MedicationId,
+                Frequency = entity.Frequency,
+                CustomFrequencyHours = entity.CustomFrequencyHours,
+                TimeOfDay = entity.TimeOfDay,
+                DaysOfWeek = entity.DaysOfWeek,
+                IsActive = entity.IsActive,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            _dbContext.MedicationSchedules.Add(medicationSchedule);
+            await _dbContext.SaveChangesAsync();
+
+            return medicationSchedule;
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _dbContext.MedicationSchedules.CountAsync();
+        }
+
+        public async Task DeleteAsync(MedicationSchedule entity)
+        {
+            _dbContext.MedicationSchedules.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsAsync(Guid id)
+        {
+            return await _dbContext.MedicationSchedules.AnyAsync(m => m.Id == id);
+        }
+
+        public async Task<IEnumerable<MedicationSchedule>> FindAsync(Expression<Func<MedicationSchedule, bool>> predicate)
+        {
+            return await _dbContext.MedicationSchedules.Where(predicate).ToListAsync();
+        }
+
+        public async Task<IEnumerable<MedicationSchedule>> GetActiveSchedulesAsync()
+        {
+            return await _dbContext.MedicationSchedules
+         .Where(e => e.IsActive) 
+         .ToListAsync();
+        }
+
+        public async Task<IEnumerable<MedicationSchedule>> GetAllAsync()
+        {
+            return await _dbContext.MedicationSchedules.ToListAsync();
+        }
+
+        public async Task<MedicationSchedule?> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.MedicationSchedules.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<MedicationSchedule>> GetByMedicationIdAsync(Guid medicationId)
+        {
+            return await _dbContext.MedicationSchedules
+                 .Include(e => e.Medication)
+               .Where(m => m.MedicationId == medicationId)
+               .OrderByDescending(m => m.CreatedAt)
+               .ToListAsync();
+        }
+
+        public async Task<IEnumerable<MedicationSchedule>> GetSchedulesByFrequencyAsync(FrequencyType frequency)
+        {
+            return await _dbContext.MedicationSchedules
+               .Where(m => m.Frequency == frequency)
+               .OrderByDescending(m => m.CreatedAt)
+               .ToListAsync();
+        }
+
+        public async Task<IEnumerable<MedicationSchedule>> GetUpcomingSchedulesAsync(DateTime startTime, DateTime endTime)
+        {
+            var schedules = await _dbContext.MedicationSchedules
+        .Where(s => s.IsActive)
+        .ToListAsync();
+            var upcoming = schedules.Where(s =>
+            {
+                var scheduledDateTime = startTime.Date + s.TimeOfDay;
+                return scheduledDateTime >= startTime && scheduledDateTime <= endTime;
+            });
+            return upcoming;
+        }
+
+        public async Task UpdateAsync(MedicationSchedule entity)
+        {
+            entity.UpdatedAt = DateTime.UtcNow;
+            _dbContext.MedicationSchedules.Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
